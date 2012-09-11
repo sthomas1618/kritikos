a18464e20444271f41eb103b5f963d1837aba331
Stephen Thomas <sthomas1618@gmail.com>
2012-09-11 10:04:01 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kritikos::Application.config.secret_token = 'bd79147552d5ed31794929644b93c1b85fbac8cb40110e47839cd34c0ddc1a93c67d04c5273d78416ac72ce5b71e8cc57344bd5bf87ef64b55a8ba3b5e152b19'
