61f2ab7f216600ceab474d915a812460b65249c6
Stephen Thomas <sthomas1618@gmail.com>
2012-10-08 17:02:06 -0700

    Fix user sign up specs and add require bootstrap js

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -49,7 +49,7 @@ describe "UserPages" do
         let(:user) { User.find_by_email('user@example.com') }
 
         #it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome  to Kritikos!') }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome to Kritikos!') }
         it { should have_link('Sign out') }
       end
     end
