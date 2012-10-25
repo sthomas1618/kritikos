e9fe19bce6adfae43afd0386d4d6d111594e182b
Stephen Thomas <sthomas1618@gmail.com>
2012-10-24 21:11:45 -0700

    Add fix for running backbone_sync_rails with models in tasks or console

--- lib/backbone_sync.rb
+++ lib/backbone_sync.rb
@@ -3,6 +3,14 @@ require "uri"
 
 module BackboneSync
 	module Rails
+		NET_HTTP_EXCEPTIONS = [Timeout::Error, Errno::ETIMEDOUT, Errno::EINVAL,
+                           Errno::ECONNRESET, Errno::ECONNREFUSED, EOFError,
+                           Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError,
+                           Net::ProtocolError]
+
+    class Engine < ::Rails::Engine
+    end
+
 		module Faye
 			
 			mattr_accessor :root_address
@@ -10,14 +18,37 @@ module BackboneSync
 
 			module Observer
 				def after_update(model)
-					Event.new(model, :update).broadcast
+					begin
+						Event.new(model, :update).broadcast
+					rescue *NET_HTTP_EXCEPTIONS => e
+            handle_net_http_exception(e)
+          end
 				end
+
 				def after_create(model)
-					Event.new(model, :create).broadcast
+					begin
+						Event.new(model, :create).broadcast
+					rescue *NET_HTTP_EXCEPTIONS => e
+            handle_net_http_exception(e)
+          end
 				end
+
 				def after_destroy(model)
-					Event.new(model, :destroy).broadcast
+					begin
+						Event.new(model, :destroy).broadcast
+					rescue *NET_HTTP_EXCEPTIONS => e
+            handle_net_http_exception(e)
+          end
 				end
+
+				def handle_net_http_exception(exception)
+          ::Rails.logger.error("")
+          ::Rails.logger.error("Backbone::Sync::Rails::Faye::Observer encountered an exception:")
+          ::Rails.logger.error(exception.class.name)
+          ::Rails.logger.error(exception.message)
+          ::Rails.logger.error(exception.backtrace.join("\n"))
+          ::Rails.logger.error("")
+        end
 			end
 
 			class Event
@@ -26,6 +57,7 @@ module BackboneSync
 					@model = model
 					@event = event
 				end
+
 				def broadcast
 					Net::HTTP.post_form(uri, :message => message)
 				end
@@ -35,13 +67,16 @@ module BackboneSync
 				def uri
 					URI.parse("#{BackboneSync::Rails::Faye.root_address}/faye")
 				end
+
 				def message
 					{ :channel => channel,
 						:data => data 			}.to_json
 				end
+
 				def channel
 					"/sync/#{@model.class.table_name}"
 				end
+
 				def data
 					{ @event => { @model.id => @model.as_json } }
 				end
